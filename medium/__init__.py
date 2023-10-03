6bd5e0b5e050e2b1a45fc720855c9c45d6407f15
meevaldas <meevadas13@gmail.com>
2023-10-03 13:57:31 +0530

    feat: coded the init.py file and the spider posts.py

---
+++ medium/__init__.py
@@ -0,0 +1,21 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+from medium.spiders.posts import PostsSpider
+
+
+def main():
+    process = CrawlerProcess(
+        settings={
+            "FEEDS": {
+                "medium.json": {"format": "json"},
+            },
+        }
+    )
+
+    process.crawl(PostsSpider)
+    process.start()  # the script will block here until the crawling is finished
+
+
+if __name__ == "__main__":
+    main()
